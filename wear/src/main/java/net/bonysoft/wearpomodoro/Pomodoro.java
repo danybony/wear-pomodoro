fdfd4a83bcf805c811bc36242fd92bdfd7734e63
Daniele Bonaldo <dany.bony@gmail.com>
2014-09-11 01:05:05 +0100

    First code drop

---
+++ wear/src/main/java/net/bonysoft/wearpomodoro/Pomodoro.java
@@ -0,0 +1,153 @@
+package net.bonysoft.wearpomodoro;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Pomodoro implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String KEY_START = "net.bonysoft.wearpomodoro.KEY_START";
+    private static final String KEY_CURRENT_STOPPAGE = "net.bonysoft.wearpomodoro.KEY_CURRENT_STOPPAGE";
+    private static final String KEY_TOTAL_STOPPAGES = "net.bonysoft.wearpomodoro.KEY_TOTAL_STOPPAGES";
+    private static final String KEY_END = "net.bonysoft.wearpomodoro.KEY_END";
+    private static final String PREFERENCES = "Pomodoro";
+
+    private static final int WORK_INTERVAL_MINUTES = 25;
+    private static final int SMALL_BREAK_MINUTES = 5;
+    private static final int LONG_BREAK_MINUTES = 15;
+    private static final int POMODORI_BEFORE_LONG_BREAK = 4;
+    private static final int MINUTE_MILLIS = 60000;
+
+    private long start;
+    private long currentStoppage;
+    private long totalStoppages;
+    private long end;
+
+    private final SharedPreferences preferences;
+
+    public static Pomodoro newInstance(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        long start = preferences.getLong(KEY_START, 0);
+        long currentStoppage = preferences.getLong(KEY_CURRENT_STOPPAGE, 0);
+        long totalStoppages = preferences.getLong(KEY_TOTAL_STOPPAGES, 0);
+        long end = preferences.getLong(KEY_END, 0);
+        return new Pomodoro(preferences, start, currentStoppage, totalStoppages, end);
+    }
+
+    private Pomodoro(SharedPreferences preferences, long start, long currentStoppage, long totalStoppages, long end) {
+        this.preferences = preferences;
+        this.start = start;
+        this.currentStoppage = currentStoppage;
+        this.totalStoppages = totalStoppages;
+        this.end = end;
+    }
+
+    public void start() {
+        if (end > 0) {
+            start = System.currentTimeMillis() - (end - start);
+            end = 0;
+        } else {
+            start = System.currentTimeMillis();
+        }
+        save();
+    }
+
+    public void stop() {
+        if (isPaused()) {
+            resume();
+        }
+        end = System.currentTimeMillis();
+        save();
+    }
+
+    public void pause() {
+        currentStoppage = System.currentTimeMillis();
+        save();
+    }
+
+    public void resume() {
+        totalStoppages += System.currentTimeMillis() - currentStoppage;
+        currentStoppage = 0L;
+        save();
+    }
+
+    public void reset() {
+        resetWithoutSave();
+        save();
+    }
+
+    private void resetWithoutSave() {
+        start = 0L;
+        currentStoppage = 0L;
+        totalStoppages = 0L;
+        end = 0L;
+    }
+
+    public long getElapsed() {
+        if (isRunning()) {
+            return System.currentTimeMillis() - start;
+        }
+        if (end > 0) {
+            return end - start;
+        }
+        return 0;
+    }
+
+    public boolean isRunning() {
+        return start > 0 && end == 0;
+    }
+
+    public boolean isPaused() {
+        return currentStoppage > 0;
+    }
+
+    public int getElapsedMinutes() {
+        return (int) ((System.currentTimeMillis() - start) / MINUTE_MILLIS);
+    }
+
+    public long getTotalStoppages() {
+        long now = System.currentTimeMillis();
+        if (isPaused()) {
+            return totalStoppages + (now - currentStoppage);
+        }
+        return totalStoppages;
+    }
+
+    public long getPlayed() {
+        return getElapsed() - getTotalStoppages();
+    }
+
+    public long getStartTime() {
+        return start;
+    }
+
+    public void save() {
+        preferences.edit()
+                .putLong(KEY_START, start)
+                .putLong(KEY_CURRENT_STOPPAGE, currentStoppage)
+                .putLong(KEY_TOTAL_STOPPAGES, totalStoppages)
+                .putLong(KEY_END, end)
+                .apply();
+    }
+
+    public void registerForUpdates() {
+        preferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    public void unregisterForUpdates() {
+        preferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        long value = sharedPreferences.getLong(key, 0L);
+        if (key.equals(KEY_START)) {
+            start = value;
+        } else if (key.equals(KEY_END)) {
+            end = value;
+        } else if (key.equals(KEY_CURRENT_STOPPAGE)) {
+            currentStoppage = value;
+        } else if (key.equals(KEY_TOTAL_STOPPAGES)) {
+            totalStoppages = value;
+        }
+    }
+}
---
+++ wear/src/main/java/net/bonysoft/wearpomodoro/PomodoroReceiver.java
@@ -0,0 +1,161 @@
+package net.bonysoft.wearpomodoro;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationManagerCompat;
+
+public class PomodoroReceiver extends BroadcastReceiver {
+    public static final String ACTION_START = "net.bonysoft.wearpomodoro.ACTION_START";
+    public static final String ACTION_STOP = "net.bonysoft.wearpomodoro.ACTION_STOP";
+    public static final String ACTION_PAUSE = "net.bonysoft.wearpomodoro.ACTION_PAUSE";
+    public static final String ACTION_RESUME = "net.bonysoft.wearpomodoro.ACTION_RESUME";
+    public static final String ACTION_RESET = "net.bonysoft.wearpomodoro.ACTION_RESET";
+    public static final String ACTION_UPDATE = "net.bonysoft.wearpomodoro.ACTION_UPDATE";
+    public static final String ACTION_ELAPSED_ALARM = "net.bonysoft.wearpomodoro.ACTION_ELAPSED_ALARM";
+    public static final String ACTION_FULL_TIME_ALARM = "net.bonysoft.wearpomodoro.ACTION_FULL_TIME_ALARM";
+
+    public static final int NOTIFICATION_ID = 1;
+
+    private static final long[] ELAPSED_PATTERN = {0, 500, 250, 500, 250, 500};
+    private static final long[] FULL_TIME_PATTERN = {0, 1000, 500, 1000, 500, 1000};
+
+    private static final int MINUTE_MILLIS = 60000;
+    private static final long DURATION = 45 * MINUTE_MILLIS;
+
+    private static final Intent UPDATE_INTENT = new Intent(ACTION_UPDATE);
+    private static final Intent ELAPSED_ALARM = new Intent(ACTION_ELAPSED_ALARM);
+    private static final Intent FULL_TIME_ALARM = new Intent(ACTION_FULL_TIME_ALARM);
+
+    private static final int REQUEST_UPDATE = 1;
+    private static final int REQUEST_ELAPSED = 2;
+    private static final int REQUEST_FULL_TIME = 3;
+
+    public static void setUpdate(Context context) {
+        context.sendBroadcast(UPDATE_INTENT);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Pomodoro timer = Pomodoro.newInstance(context);
+        boolean shouldUpdate = false;
+        if (intent.getAction().equals(ACTION_UPDATE)) {
+            shouldUpdate = true;
+        } else if (intent.getAction().equals(ACTION_START)) {
+            start(context, timer);
+            shouldUpdate = true;
+        } else if (intent.getAction().equals(ACTION_STOP)) {
+            stop(context, timer);
+            shouldUpdate = true;
+        } else if (intent.getAction().equals(ACTION_PAUSE)) {
+            pause(context, timer);
+            shouldUpdate = true;
+        } else if (intent.getAction().equals(ACTION_RESUME)) {
+            resume(context, timer);
+            shouldUpdate = true;
+        } else if (intent.getAction().equals(ACTION_RESET)) {
+            reset(timer);
+        } else if (intent.getAction().equals(ACTION_ELAPSED_ALARM)) {
+            elapsedAlarm(context);
+        } else if (intent.getAction().equals(ACTION_FULL_TIME_ALARM)) {
+            fullTimeAlarm(context);
+        }
+
+        if (shouldUpdate) {
+            updateNotification(context, timer);
+        }
+    }
+
+    private void updateNotification(Context context, Pomodoro timer) {
+        NotificationBuilder builder = new NotificationBuilder(context, timer);
+        Notification notification = builder.buildNotification();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void reset(Pomodoro pomodoro) {
+        pomodoro.reset();
+    }
+
+    private void resume(Context context, Pomodoro pomodoro) {
+        pomodoro.resume();
+        long playedEnd = pomodoro.getStartTime() + pomodoro.getTotalStoppages() + DURATION;
+        if (playedEnd > System.currentTimeMillis()) {
+            setAlarm(context, REQUEST_FULL_TIME, FULL_TIME_ALARM, playedEnd);
+        }
+    }
+
+    private void pause(Context context, Pomodoro pomodoro) {
+        pomodoro.pause();
+        cancelAlarm(context, REQUEST_FULL_TIME, FULL_TIME_ALARM);
+        long elapsedEnd = pomodoro.getStartTime() + DURATION;
+        if (!isAlarmSet(context, REQUEST_ELAPSED, ELAPSED_ALARM) && elapsedEnd > System.currentTimeMillis()) {
+            setAlarm(context, REQUEST_ELAPSED, ELAPSED_ALARM, elapsedEnd);
+        }
+    }
+
+    private void stop(Context context, Pomodoro pomodoro) {
+        pomodoro.stop();
+        cancelAlarm(context, REQUEST_UPDATE, UPDATE_INTENT);
+        cancelAlarm(context, REQUEST_ELAPSED, ELAPSED_ALARM);
+        cancelAlarm(context, REQUEST_FULL_TIME, FULL_TIME_ALARM);
+    }
+
+    private void start(Context context, Pomodoro pomodoro) {
+        pomodoro.start();
+        long elapsedEnd = pomodoro.getStartTime() + DURATION;
+        setRepeatingAlarm(context, REQUEST_UPDATE, UPDATE_INTENT);
+        if (pomodoro.getTotalStoppages() > 0 && !pomodoro.isPaused()) {
+            long playedEnd = pomodoro.getStartTime() + pomodoro.getTotalStoppages() + DURATION;
+            if (playedEnd > System.currentTimeMillis()) {
+                setAlarm(context, REQUEST_FULL_TIME, FULL_TIME_ALARM, playedEnd);
+            }
+            if (elapsedEnd > System.currentTimeMillis()) {
+                setAlarm(context, REQUEST_ELAPSED, ELAPSED_ALARM, elapsedEnd);
+            }
+        } else {
+            if (elapsedEnd > System.currentTimeMillis()) {
+                setAlarm(context, REQUEST_FULL_TIME, FULL_TIME_ALARM, elapsedEnd);
+            }
+        }
+    }
+
+    private void setRepeatingAlarm(Context context, int requestCode, Intent intent) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), MINUTE_MILLIS, pendingIntent);
+    }
+
+    private boolean isAlarmSet(Context context, int requestCode, Intent intent) {
+        return PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_NO_CREATE) != null;
+    }
+
+    private void setAlarm(Context context, int requestCode, Intent intent, long time) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, pendingIntent);
+    }
+
+    private void cancelAlarm(Context context, int requestCode, Intent intent) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_NO_CREATE);
+        if (pendingIntent != null) {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager.cancel(pendingIntent);
+            pendingIntent.cancel();
+        }
+    }
+
+    private void elapsedAlarm(Context context) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(ELAPSED_PATTERN, -1);
+    }
+
+    private void fullTimeAlarm(Context context) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(FULL_TIME_PATTERN, -1);
+    }
+}
