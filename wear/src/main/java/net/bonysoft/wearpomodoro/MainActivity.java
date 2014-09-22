7e72b9639ed5a2307081f8001a29375fbf8261d9
Daniele Bonaldo <dany.bony@gmail.com>
2014-09-22 23:06:57 +0100

    Start the pomodoro, if not already running, on MainActivity opened

--- wear/src/main/java/net/bonysoft/wearpomodoro/MainActivity.java
+++ wear/src/main/java/net/bonysoft/wearpomodoro/MainActivity.java
@@ -3,8 +3,6 @@ package net.bonysoft.wearpomodoro;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
@@ -14,21 +12,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                startButton = (ImageButton) stub.findViewById(R.id.button_start);
-                startButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent startIntent = new Intent(PomodoroReceiver.ACTION_START);
-                        sendBroadcast(startIntent);
-                        finish();
-                    }
-                });
-            }
-        });
+        PomodoroTimer pomodoroTimer = PomodoroTimer.newInstance(this);
+        if (pomodoroTimer.isRunning()) {
+            PomodoroReceiver.setUpdate(this);
+        } else {
+            Intent startIntent = new Intent(PomodoroReceiver.ACTION_START);
+            sendBroadcast(startIntent);
+        }
+        finish();
     }
 }
